f2a64de09e9138db62fa8ebcaf1ccccd7af34ae4
Yakimov1337 <92337248+Yakimov1337@users.noreply.github.com>
2022-11-09 14:18:45 +0200

    Main section

--- pages/index.tsx
+++ pages/index.tsx
@@ -62,13 +62,13 @@ const Home = ({
   topRated,
   trendingNow,
 }: Props) => {
-  console.log(netflixOriginals,  actionMovies,
-    comedyMovies,
-    documentaries,
-    horrorMovies,
-    romanceMovies,
-    topRated,
-    trendingNow,);
+  // console.log(netflixOriginals,  actionMovies,
+  //   comedyMovies,
+  //   documentaries,
+  //   horrorMovies,
+  //   romanceMovies,
+  //   topRated,
+  //   trendingNow,);
   return (
     <div className="relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511] lg:h-[140vh]">
       <Head>
@@ -76,8 +76,8 @@ const Home = ({
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
-      <main>
-        <Banner />
+      <main className="relative lg:pt-16 pl-8 pb-24 lg:space-y-24">
+        <Banner netflixOriginals={netflixOriginals} />
         <section>{/* Row */}</section>
       </main>
       {/* Modal */}
