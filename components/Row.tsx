0ba4e1e381b5c549b7e80dbc4da66d5c5f3b7c5a
Yakimov1337 <92337248+Yakimov1337@users.noreply.github.com>
2022-11-10 11:14:52 +0200

    Row chevrons

--- components/Row.tsx
+++ components/Row.tsx
@@ -1,9 +1,20 @@
-import React from 'react'
-
-function Row() {
+import { ChevronLeftIcon } from "@heroicons/react/24/solid";
+import { ChevronRightIcon } from "@heroicons/react/24/solid";
+import { Movie } from "../typings";
+interface Props {
+  title: string;
+  movies: Movie[];
+}
+function Row({ title, movies }: Props) {
   return (
-    <div>Row</div>
-  )
+    <div>
+      <h2>{title}</h2>
+      <div className="group">
+        <ChevronLeftIcon className="absolute top-0 bottom-0 left-2 z-40 m-auto h-9 w-9 cursor-pointer opacity-0 transition hover:scale-125 group-hover:opacity-100"/>
+        <ChevronRightIcon className="absolute top-0 bottom-0 left-2 z-40 m-auto h-9 w-9 cursor-pointer opacity-0 transition hover:scale-125 group-hover:opacity-100"/>
+      </div>
+    </div>
+  );
 }
 
-export default Row
+export default Row;
