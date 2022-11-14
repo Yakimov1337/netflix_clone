6fa2a143cac53e99f2cd8abb4f0716835a499921
Yakimov1337 <92337248+Yakimov1337@users.noreply.github.com>
2022-11-14 15:14:30 +0200

    logout handle

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,9 +2,11 @@ import { MagnifyingGlassIcon } from "@heroicons/react/24/solid";
 import { BellIcon } from "@heroicons/react/24/solid";
 import Link from "next/link";
 import { useEffect, useState } from "react";
+import useAuth from "../hooks/useAuth";
 
 function Header() {
   const [isScrolled, setIsScrolled] = useState(false);
+  const { logout } = useAuth();
 
   useEffect(() => {
     const handleScroll = () => {
@@ -15,18 +17,18 @@ function Header() {
       }
     };
 
-    window.addEventListener("scroll",handleScroll)
+    window.addEventListener("scroll", handleScroll);
 
-    return() => {
-      window.removeEventListener('scroll',handleScroll)
-    }
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
   }, []);
 
   return (
-    <header className={`${isScrolled && 'bg-[#141414]'}`}>
+    <header className={`${isScrolled && "bg-[#141414]"}`}>
       <div className="flex items-center space-x-2 md:space-x-10">
         <img
-          src="https://rb.gy/ulxxee"
+          src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg"
           width={100}
           height={100}
           className="cursor-pointer object-contain"
@@ -46,13 +48,12 @@ function Header() {
         <MagnifyingGlassIcon className="hidden h-6 w-6 sm:inline" />
         <p className="hidden lg:inline">Kids</p>
         <BellIcon className="h-6 w-6" />
-        <Link href="/account">
-          <img
-            src="https://rb.gy/g1pwyx"
-            alt=""
-            className="cursor-pointer rounded"
-          />
-        </Link>
+        <img
+          onClick={logout}
+          src="https://rb.gy/g1pwyx"
+          alt=""
+          className="cursor-pointer rounded"
+        />
       </div>
     </header>
   );
