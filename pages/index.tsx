77c008678d63fda60003016cc4b290468d5f1465
Yakimov1337 <92337248+Yakimov1337@users.noreply.github.com>
2022-11-14 15:15:30 +0200

    handle loading

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,6 +6,7 @@ import Banner from "../components/Banner";
 import requests from "../utils/requests";
 import { Movie } from "../typings";
 import Row from "../components/Row";
+import useAuth from "../hooks/useAuth";
 
 export const getServerSideProps = async () => {
   const [
@@ -63,13 +64,11 @@ const Home = ({
   topRated,
   trendingNow,
 }: Props) => {
-  // console.log(netflixOriginals,  actionMovies,
-  //   comedyMovies,
-  //   documentaries,
-  //   horrorMovies,
-  //   romanceMovies,
-  //   topRated,
-  //   trendingNow,);
+  const { logout, loading } = useAuth();
+
+  if (loading) {
+   return <h1>Loading</h1>;
+  }
   return (
     <div className="relative h-screen bg-gradient-to-b lg:h-[140vh]">
       <Head>
@@ -80,13 +79,13 @@ const Home = ({
       <main className="relative lg:pt-16 pl-8 pb-24 lg:space-y-24">
         <Banner netflixOriginals={netflixOriginals} />
         <section className="md:space-y-24">
-          <Row title="Trending Now" movies={trendingNow}/>
-          <Row title="Top Rated" movies={topRated}/>
-          <Row title="Action Thrillers" movies={actionMovies}/>
-          <Row title="Comedies" movies={comedyMovies}/>
-          <Row title="Scary Movies" movies={horrorMovies}/>
-          <Row title="Romance Movies" movies={romanceMovies}/>
-          <Row title="Documentaries" movies={documentaries}/>
+          <Row title="Trending Now" movies={trendingNow} />
+          <Row title="Top Rated" movies={topRated} />
+          <Row title="Action Thrillers" movies={actionMovies} />
+          <Row title="Comedies" movies={comedyMovies} />
+          <Row title="Scary Movies" movies={horrorMovies} />
+          <Row title="Romance Movies" movies={romanceMovies} />
+          <Row title="Documentaries" movies={documentaries} />
         </section>
       </main>
       {/* Modal */}
