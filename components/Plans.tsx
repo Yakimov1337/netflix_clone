aab8101a2341c56f6bdc9b137ac6fb14f063ec10
Yakimov1337 <92337248+Yakimov1337@users.noreply.github.com>
2022-11-15 14:08:05 +0200

    Hardcoded plans

---
+++ components/Plans.tsx
@@ -0,0 +1,65 @@
+import Head from "next/head";
+import Link from "next/link";
+import React from "react";
+import useAuth from "../hooks/useAuth";
+import { CheckIcon } from "@heroicons/react/24/outline";
+
+function Plans() {
+  const { logout } = useAuth();
+
+  return (
+    <div>
+      <Head>
+        <title>Netflix</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <header className="border-b border-white/10 bg-[#141414]">
+        <Link href="/">
+          <img
+            src="https://rb.gy/ulxxee"
+            alt="Netflix"
+            width={150}
+            height={90}
+            className="cursor-pointer object-contain"
+          />
+        </Link>
+        <button
+          onClick={logout}
+          className="text-lg font-medium hover:underline"
+        >
+          Sign Out
+        </button>
+      </header>
+      <main className="pt-28 ">
+        <h1 className="mb-3 text-3xl font-medium">
+          Choose the plan that's right for you
+        </h1>
+        <ul>
+          <li className="flex items-center gap-x-2 text-lg">
+            <CheckIcon className="h-7 w-7 text-[#E50914]" /> Watch all you want.
+            Ad-free.
+          </li>
+          <li className="flex items-center gap-x-2 text-lg">
+            <CheckIcon className="h-7 w-7 text-[#E50914]" /> Recommendations
+            just for you.
+          </li>
+          <li className="flex items-center gap-x-2 text-lg">
+            <CheckIcon className="h-7 w-7 text-[#E50914]" /> Change or cancel
+            your plan anytime.
+          </li>
+        </ul>
+
+        <div className="mt-4 flex flex-col space-y-4">
+          <div className="flex w-full items-center self-end md:w-3/5">
+            <div className="planBox">standard</div>
+            <div className="planBox">standard</div>
+            <div className="planBox">standard</div>
+
+          </div>
+        </div>
+      </main>
+    </div>
+  );
+}
+
+export default Plans;
