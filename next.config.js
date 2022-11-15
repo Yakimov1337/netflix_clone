5e5c6ed88616e71200e147188c16ef39ace0ef37
Yakimov1337 <92337248+Yakimov1337@users.noreply.github.com>
2022-11-15 14:23:33 +0200

    next transpile modules

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
+const withTM=require('next-transpile-modules')([
+  '@stripe/firestore-stripe-payments',
+])
 /** @type {import('next').NextConfig} */
-module.exports = {
+module.exports = withTM ({
   reactStrictMode: true,
   images: {
     domains: ['rb.gy', 'image.tmdb.org','upload.wikimedia.org'],
   }
-}
+})
